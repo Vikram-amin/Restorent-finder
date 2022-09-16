cdb8fbd5448f3bfa9ec1c93bb9b6f441be0e4a7a
Vikram <vikramamin22@gmail.com>
2022-09-16 20:45:02 +0530

    middleware custum and morgan

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,35 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
+
 const PORT = process.env.PORT || 3001;
-app.use(epress.json());
+
+app.use(express.json());
+app.use(morgan("tiny"));
+
+
+//  custum midddleware
+app.use((req, res, next) => {
+  console.log("middleware");
+  next();
+});
+
+app.get("/api/v1/getRestorents", (req, res) => {
+  console.log("hi");
+  res.status(404).json({
+    status: "success",
+    restaurant: "KFC",
+  });
+});
+
+app.get("/api/v1/getRestorents/:id", (req, res) => {
+  console.log(res.params);
+});
 
 
 
-
-app.listen(PORT , ()=>{
-    console.log(` server listoning at port ${PORT}`);
-})
-
+app.listen(PORT, () => {
+  console.log(` server listening at port ${PORT}`);
+});
